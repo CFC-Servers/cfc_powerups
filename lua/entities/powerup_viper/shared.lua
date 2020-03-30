441b305c4b27b1e18cf151d2887e94d91b8a6822
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-03-30 05:27:29 -0400

    Remove compiled lua

--- lua/entities/powerup_viper/shared.lua
+++
@@ -1,6 +0,0 @@
-ENT.Base = "base_cfc_powerup"
-ENT.Type = "anim"
-ENT.PrintName = "Viper Powerup"
-ENT.Purpose = "Cloaks the user to near-invisibility, does significantly more damage with melee weapons"
-ENT.Spawnable = true
-ENT.AdminOnly = true
